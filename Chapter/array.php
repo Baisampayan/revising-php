ed1963ec787f446d3030b732affdfee67885c5b3
Baisampayan Das <baisampayandas@gmail.com>
2025-05-28 01:16:30 +0530

    Adding Array

---
+++ Chapter/array.php
@@ -0,0 +1,13 @@
+<?php
+
+// Two ways to define Array
+$a = [1,2,3,4,5];
+$b = array(6,7,8,9,10);
+
+// Printing An Array
+print_r($a);
+
+echo "<br><br>";
+
+// Printing An Array with Details
+var_dump($b);
